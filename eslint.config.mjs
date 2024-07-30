cfd6957b8615146f32d739b6680e6230b7ac4ad4
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2024-07-31 03:13:32 +0900

    add type checking

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
 
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { ignores: ["dist"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { ignores: ['dist'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   eslintConfigPrettier,
 ];
