cd4a22b573b880331c4ec499091c516f7190cc9b
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2024-07-31 02:57:20 +0900

    add template

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["dist"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+];
